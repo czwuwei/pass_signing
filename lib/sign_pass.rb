79cf6cc05049926ee6c9c152b1b03b0b53fffcbe
czwuwei <wu-wei@cyber-z.co.jp>
2017-03-17 14:06:28 +0900

    Merge with Apple original source

--- lib/sign_pass.rb
+++ lib/sign_pass.rb
@@ -56,8 +56,8 @@ require 'zip'
 
 class SignPass
   attr_accessor :pass_url, :certificate_url, :certificate_password, :output_url, :compress_into_zip_file, :temporary_directory, :temporary_path, :manifest_url, :signature_url, :wwdr_intermediate_certificate_path
-  
-  
+
+
   def initialize(pass_url, certificate_url, certificate_password, wwdr_intermediate_certificate_path, output_url, compress_into_zip_file=true)
     self.pass_url                           = pass_url
     self.certificate_url                    = certificate_url
@@ -66,99 +66,99 @@ class SignPass
     self.output_url                         = output_url
     self.compress_into_zip_file             = compress_into_zip_file
   end
-  
-  
+
+
   def sign_pass!(force_clean_raw_pass=false)
     # Validate that requested contents are not a signed and expanded pass archive.
     self.validate_directory_as_unsigned_raw_pass(force_clean_raw_pass)
-    
+
     # Get a temporary place to stash the pass contents
     self.create_temporary_directory
-    
+
     # Make a copy of the pass contents to the temporary folder
     self.copy_pass_to_temporary_location
-    
+
     # Clean out the unneeded .DS_Store files
     self.clean_ds_store_files
-    
+
     # Build the json manifest
     self.generate_json_manifest
-    
+
     # Sign the manifest
     self.sign_manifest
-    
+
     # Package pass
     self.compress_pass_file
-    
+
     # Clean up the temp directory
     #self.delete_temp_dir
   end
-  
-  
+
+
   # private
-  
+
   # Ensures that the raw pass directory does not contain signatures
   def validate_directory_as_unsigned_raw_pass(force_clean=false)
     if force_clean
       force_clean_raw_pass
     end
-    
+
     has_manifiest = File.exists?(File.join(self.pass_url, "/manifest.json"))
     puts "Raw pass has manifest? #{has_manifiest}"
-    
+
     has_signiture = File.exists?(File.join(self.pass_url, "/signature"))
     puts "Raw pass has signature? #{has_signiture}"
-        
+
     if has_signiture || has_manifiest
       raise RuntimeError, "#{self.pass_url} contains pass signing artificats that need to be removed before signing."
-      
+
     end
   end
-  
+
   def force_clean_raw_pass
     puts "Force cleaning the raw pass directory."
     if File.exists?(File.join(self.pass_url, "/manifest.json"))
       File.delete(File.join(self.pass_url, "/manifest.json"))
     end
-    
+
     if File.exists?(File.join(self.pass_url, "/signature"))
       File.delete(File.join(self.pass_url, "/signature"))
     end
-    
+
   end
-  
-  
+
+
   # Creates a temporary place to work with the pass files without polluting the original
   def create_temporary_directory
     self.temporary_directory = Dir.mktmpdir
     puts "Creating temp dir at #{self.temporary_directory}"
     self.temporary_path = self.temporary_directory + "/" + self.pass_url.split("/").last
-    
+
     # Check if the directory exists
     if File.directory?(self.temporary_path)
       # Need to clean up the directory
       FileUtils.rm_rf(self.temporary_path)
     end
-    
+
   end
-  
+
   # Copies the pass contents to the temporary location
   def copy_pass_to_temporary_location
     puts "Copying pass to temp directory."
     FileUtils.cp_r(self.pass_url, self.temporary_directory)
   end
-  
-  
+
+
   # Removes .DS_Store files if they exist
   def clean_ds_store_files
     puts "Cleaning .DS_Store files"
     Dir.glob(self.temporary_path + "**/.DS_Store").each do |file|
       File.delete(file)
     end
-    
+
   end
-  
-  
+
+
   # Creates a json manifest where each files contents has a SHA1 hash
   def generate_json_manifest
     puts "Generating JSON manifest"
@@ -167,38 +167,38 @@ class SignPass
     Dir.glob(self.temporary_path + "/**").each do |file|
       manifest[File.basename(file)] = Digest::SHA1.hexdigest(File.read(file))
     end
-    
+
     # Write the hash dictionary out to a manifest file
     self.manifest_url = self.temporary_path + "/manifest.json"
     File.open(self.manifest_url, "w") do |f|
       f.write(manifest.to_json)
     end
-    
+
   end
-  
+
   def sign_manifest
     puts "Signing the manifest"
     # Import the certificates
     p12_certificate = OpenSSL::PKCS12::new(File.read(self.certificate_url), self.certificate_password)
     wwdr_certificate = OpenSSL::X509::Certificate.new(File.read(self.wwdr_intermediate_certificate_path))
-    
+
     # Sign the data
     flag = OpenSSL::PKCS7::BINARY|OpenSSL::PKCS7::DETACHED
     signed = OpenSSL::PKCS7::sign(p12_certificate.certificate, p12_certificate.key, File.read(self.manifest_url), [wwdr_certificate], flag)
-    
+
     # Create an output path for the signed data
     self.signature_url = self.temporary_path + "/signature"
-    
+
     # Write out the data
     File.open(self.signature_url, "w") do |f|
       f.syswrite signed.to_der
     end
   end
-  
+
   def compress_pass_file
     puts "Compressing the pass"
     zipped_file = File.open(self.output_url, "w")
-    
+
     Zip::OutputStream.open(zipped_file.path) do |z|
       Dir.glob(self.temporary_path + "/**").each do |file|
         z.put_next_entry(File.basename(file))
@@ -207,22 +207,8 @@ class SignPass
     end
     zipped_file
   end
-  
+
   def delete_temp_dir
     FileUtils.rm_rf(self.temporary_path)
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
